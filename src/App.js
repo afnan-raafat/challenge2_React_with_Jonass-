14cd62cb45686137337f83fa2f8b7a5d18bbf16e
afnan raafat <121581251+afnan-raafat@users.noreply.github.com>
2024-10-14 03:30:11 +0300

    challange 2 react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+//  this is practise on Event handeler on react
+import { useState } from "react";
+import "./index.css";
+const messages = [
+  "mix flour, eggs, sugar, and butter 🥰",
+  "our the mixture into a baking tin 🤏",
+  "bake at 180°C and eat it 😋",
+];
 
-function App() {
+export default function App() {
+  const [step, setStep] = useState(1);
+  //  هنعمل 2 فانكشن هنهندلهم للازرار
+  function handlePrev() {
+    if (step > 1) setStep(step - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) setStep(step + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="steps">
+      <div className="textHeader">
+        <h1>Steps To Prepare a Cake With Me 🍰</h1>
+        <br></br>
+      </div>
+
+      {/* Numbers  */}
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+      {/* Message */}
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+      {/* Buttons */}
+
+      <div className="buttons">
+        <button onClick={handlePrev}>Previous</button>
+        <button onClick={handleNext}>Next</button>
+      </div>
     </div>
   );
 }
-
-export default App;
